244d3efadc74f36c0f4be5163bf8726d59e43e12
Minetrio1256 <141367569+Minetrio1256@users.noreply.github.com>
2025-05-03 18:00:39 -0400

    Temperature

--- src/main/java/org/minetrio1256/craftdustry/api/energy/Temperature.java
+++ src/main/java/org/minetrio1256/craftdustry/api/energy/Temperature.java
@@ -1,4 +1,17 @@
 package org.minetrio1256.craftdustry.api.energy;
 
 public class Temperature {
+    private int temperature;
+
+    public Temperature(int temperature){
+        this.temperature = temperature;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
 }
