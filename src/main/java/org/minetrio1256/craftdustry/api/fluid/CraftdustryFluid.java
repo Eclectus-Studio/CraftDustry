93e9d5c270d01c746a0bc1fb99fc0ada377ad765
Minetrio1256 <141367569+Minetrio1256@users.noreply.github.com>
2025-05-03 18:02:41 -0400

    Temperature on fluids

--- src/main/java/org/minetrio1256/craftdustry/api/fluid/CraftdustryFluid.java
+++ src/main/java/org/minetrio1256/craftdustry/api/fluid/CraftdustryFluid.java
@@ -1,14 +1,26 @@
 package org.minetrio1256.craftdustry.api.fluid;
 
+import org.minetrio1256.craftdustry.api.energy.Temperature;
+
 public class CraftdustryFluid {
     private final String fluidType;
+    private final Temperature temperature;
 
-    public CraftdustryFluid(final String fluidType) {
+    public CraftdustryFluid(final String fluidType, Temperature temperature) {
         this.fluidType = fluidType;
+        this.temperature = temperature;
     }
 
     public String getFluidType() {
         return this.fluidType;
+    }
+
+    public Temperature getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature){
+        this.temperature.setTemperature(temperature);
     }
 }
 
