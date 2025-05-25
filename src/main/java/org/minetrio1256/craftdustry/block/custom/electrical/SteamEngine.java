345ae1bd87459d36888bf801e7cebe9a770197b4
Minetrio1256 <141367569+Minetrio1256@users.noreply.github.com>
2025-05-25 14:45:58 -0400

    Missing Block Entity Class and SteamEngine

---
+++ src/main/java/org/minetrio1256/craftdustry/block/custom/electrical/SteamEngine.java
@@ -0,0 +1,4 @@
+package org.minetrio1256.craftdustry.block.custom.electrical;
+
+public class SteamEngine {
+}
---
+++ src/main/java/org/minetrio1256/craftdustry/block/entity/custom/fluid/storage/TankBlockEntity.java
@@ -0,0 +1,4 @@
+package org.minetrio1256.craftdustry.block.entity.custom.fluid.storage;
+
+public class TankBlockEntity {
+}
