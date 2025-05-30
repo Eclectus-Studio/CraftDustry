3b967baeec22bfe1d58afb04640b5a65dbebde56
Minetrio1256 <141367569+Minetrio1256@users.noreply.github.com>
2025-05-29 20:25:26 -0400

    small datapack parsing, needs testing

--- src/main/java/org/minetrio1256/craftdustry/data/player/UnlockedCape.java
+++ src/main/java/org/minetrio1256/craftdustry/data/player/UnlockedCape.java
@@ -7,11 +7,12 @@ import java.util.*;
 public class UnlockedCape {
     private static final Map<UUID, Set<ResourceLocation>> unlockedCapes = new HashMap<>();
 
-    public static void unlockCape(UUID uuid, ResourceLocation cape) {
-        unlockedCapes.computeIfAbsent(uuid, k -> new HashSet<>()).add(cape);
+    public static void unlockCape(UUID uuid, ResourceLocation capeId) {
+        unlockedCapes.computeIfAbsent(uuid, k -> new HashSet<>()).add(capeId);
     }
 
-    public static boolean hasCape(UUID uuid, ResourceLocation cape) {
-        return unlockedCapes.getOrDefault(uuid, Set.of()).contains(cape);
+    public static boolean hasCape(UUID uuid, ResourceLocation capeId) {
+        return unlockedCapes.getOrDefault(uuid, Set.of()).contains(capeId);
     }
+
 }
